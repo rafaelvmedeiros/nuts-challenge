8d313fd1f557e0ea051c6aa460e6d359ec70770d
Rafael <rafaeldesenv.net@gmail.com>
2022-11-08 14:02:04 -0300

    feat: add dividers service

---
+++ src/util/dividers.ts
@@ -0,0 +1,7 @@
+export function numberIsDivisibleByThree(data: number):boolean {
+    return data % 3 === 0
+}
+
+export function numberIsDivisibleByFive(data: number):boolean {
+    return data % 5 === 0
+}
