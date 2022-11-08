21bdaeae0ebf772ae210a1b913ad4dba35b9cbd3
Rafael <rafaeldesenv.net@gmail.com>
2022-11-08 14:57:41 -0300

    chore: add node http mocks

---
+++ src/presentation/controllers/printController.spec.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from "express";
+import { createRequest, createResponse, MockRequest, MockResponse } from 'node-mocks-http';
+  
+import PrintController from "./printerController"
+
+describe("Print Controller", () => {
+    let sut = PrintController;
+    let request: MockRequest<Request>;
+    let response: MockResponse<Response>;
+
+    beforeEach(() => {
+        response = createResponse();
+    })
+
+    it("should be defined", () => {
+        expect(sut).toBeDefined()
+    })
+
+    describe("GET /print-numbers", () => {
+
+        it("should return 200 and data", async() => {
+            request = createRequest({
+                method: 'GET',
+                url: '/print-numbers?range=100',
+              });
+
+            sut.index(request, response), () => {
+                expect(response.getHeaders()).toHaveProperty('content-type');
+                expect(response.statusCode).toBe(200);
+                expect(response.statusMessage).toBe("OK")
+                expect(JSON.parse(response._getData())).toEqual(expect.arrayContaining([expect.any]))
+            }  
+
+        })
+
+        it("should define a default range when user doesn't input it and return status 200 and data", async() => {
+            request = createRequest({
+                method: 'GET',
+                url: '/print-numbers',
+              });
+
+              sut.index(request, response), () => {
+                expect(response.getHeaders()).toHaveProperty('content-type');
+                expect(response.statusCode).toBe(200);
+                expect(response.statusMessage).toBe("OK")
+                expect(JSON.parse(response._getData())).toEqual(expect.arrayContaining([expect.any]))
+            }
+        })
+    })
+})
--- src/presentation/controllers/printerController.ts
+++ src/presentation/controllers/printerController.ts
@@ -4,10 +4,10 @@ import { PrinterService } from "../../services/printer";
 class PrinterController {
     async index(req: Request, res: Response) {
         const printService = new PrinterService()
-        
+     
         const range = req.query.hasOwnProperty("range") ? Number(req.query.range) : 100
         const result = printService.printer(range)
-
+       
         return res.status(200).json({ data: result })
     }
 }
